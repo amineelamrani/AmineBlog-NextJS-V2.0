4c23ac1bdd3461326387713081e05247b789c113
Amine ELAMRANI <amine.elamrani.j.s.s.h@gmail.com>
2025-08-26 20:20:34 +0100

    Did the server rendering for the home page instead of calling an API like I did in the MERN project (to improve on which type of rendering and all of the stuff)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-"use client";
-import { useEffect } from "react";
+import "@/models/index";
+import Link from "next/link";
+import dbConnect from "@/lib/dbConnect";
+import Article from "@/models/Article";
 
-export default function Home() {
-  useEffect(() => {
-    const homePage = async () => {
-      const res = await fetch("/api/v2/articles", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await res.json();
-      console.log(data);
-    };
-    homePage();
-  });
-  return <div>Home page</div>;
+// so like this we would build our home page for SEO (no need to have an API to call it then do some client work like in the MERN project - so this is what is different in Nextjs)
+
+interface Props {
+  searchParams?: { page?: string };
+}
+
+export default async function Home({ searchParams }: Props) {
+  const search = await searchParams;
+  const page = parseInt(search?.page || "1");
+  console.log(page);
+  await dbConnect();
+  const queryHomeArticles = await Article.find({})
+    .select("-content")
+    .sort("-createdAt")
+    .skip((page - 1) * 6)
+    .limit(6)
+    .populate("author", "name profilePicture")
+    .exec();
+
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Link href={`?page=${2}`}>
+        <button>Load more</button>
+      </Link>
+    </div>
+  );
 }
